bee2bae70ab8994be85230f0610016ac376620f5
Stefan Berger <stefanb@linux.vnet.ibm.com>
2020-08-16 20:59:16 -0400

    Do a git pull to keep build up to date and allow forcing build
    Do a git pull if user forces a build of the package:
    
    DATE="$(date)" ./build.sh
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # to build without cache: build.sh --no-cache
+# to force a re-build of libtpms and swtpm: DATE=$(date) build.sh
 
 for df in  \
 	Dockerfile.alpine \
@@ -14,5 +15,6 @@ for df in  \
 	docker build \
 		--build-arg LIBTPMS_BRANCH=${LIBTPMS_BRANCH:-master} \
 		--build-arg SWTPM_BRANCH=${SWTPM_BRANCH:-master} \
+		--build-arg DATE="${DATE:-none}" \
 		$@ -f $df . || { echo "$fd failed."; exit 1; }
 done
