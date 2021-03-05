bcc7770901e75974d75d59134de27344b0068b00
Stefan Berger <stefanb@linux.vnet.ibm.com>
2021-03-05 11:33:25 -0500

    OpenSUSE: Convert Docker file to script file to work around install issues
    This patch is a work-around for the follwing issue. The issue seems
    to be related to Docker rather than OpenSUSE.
    
    (76/80) Installing: openssh-server-8.4p1-4.1.x86_64 [..error]
    Abort, retry, ignore? [a/r/i] (a): a
    Installation of openssh-server-8.4p1-4.1.x86_64 failed:
    Error: Subprocess failed. Error: RPM failed: find: '/etc/init.d/rc[35].d': No such file or directory
    ERROR: neither useradd nor busybox found!
    error: %prein(openssh-server-8.4p1-4.1.x86_64) scriptlet failed, exit status 1
    error: openssh-server-8.4p1-4.1.x86_64: install failed
    Warning: %posttrans scripts skipped while aborting:
        pam-1.5.1-3.2.x86_64.rpm
        shadow-4.8.1-5.2.x86_64.rpm
        openssh-server-8.4p1-4.1.x86_64.rpm
    
    The only solution seems to be to convert the Dockerfile into
    a script file and use docker run --privileged to run it.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,30 @@
 # to build without cache: build.sh --no-cache
 # to force a re-build of libtpms and swtpm: DATE=$(date) build.sh
 
+if [ -d /dev/vtpmx ]; then
+	# This creates test failures when using --privileged
+	echo "Remove tpm_vtpm_proxy from kernel."
+	exit 1
+fi
+
+for img in \
+	opensuse/tumbleweed;
+do
+	echo "------------------------------------"
+	echo ">>>>>> Running tests in $df <<<<<<"
+	set -x
+	docker run \
+		--env LIBTPMS_BRANCH=${LIBTPMS_BRANCH:-master} \
+		--env SWTPM_BRANCH=${SWPTM_BRANCH:-master} \
+		--env DATE="${DATE:-none}" \
+		--rm \
+		--privileged \
+		-v ${PWD}:/test \
+		${img} \
+		/test/test.$(echo ${img} | tr "/" "-") \
+		|| { echo "${img} failed."; exit 1; }
+done
+
 for df in  \
 	Dockerfile.alpine \
 	Dockerfile.centos \
@@ -16,5 +40,5 @@ for df in  \
 		--build-arg LIBTPMS_BRANCH=${LIBTPMS_BRANCH:-master} \
 		--build-arg SWTPM_BRANCH=${SWTPM_BRANCH:-master} \
 		--build-arg DATE="${DATE:-none}" \
-		$@ -f $df . || { echo "$fd failed."; exit 1; }
+		$@ -f $df . || { echo "$df failed."; exit 1; }
 done
